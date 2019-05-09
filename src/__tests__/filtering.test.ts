07a7397e4cea8a2380905caa49b3b197f427cabe
Nir Elbaz <nire0510@gmail.com>
2019-05-09 12:28:40 +0300

    0.0.6

--- src/__tests__/filtering.test.ts
+++ src/__tests__/filtering.test.ts
@@ -2,22 +2,57 @@ import jsoq from '../index';
 const json = require('./data.json');
 
 test('distinct', () => {
-  expect(jsoq.from(json).distinct().done().length).toBe(6);
-  expect(jsoq.from(json).distinct('gender').done().length).toBe(2);
+  expect(
+    jsoq
+      .from(json)
+      .distinct()
+      .done().length,
+  ).toBe(6);
+  expect(
+    jsoq
+      .from(json)
+      .distinct('gender')
+      .done().length,
+  ).toBe(2);
 });
 
 test('first', () => {
-  expect(jsoq.from(json).first().done().length).toBe(1);
-  expect(jsoq.from(json).first(2).done().length).toBe(2);
+  expect(
+    jsoq
+      .from(json)
+      .first()
+      .done().length,
+  ).toBe(1);
+  expect(
+    jsoq
+      .from(json)
+      .first(2)
+      .done().length,
+  ).toBe(2);
 });
 
 test('in', () => {
-  expect(jsoq.from(json).in('age', [31, 37]).done().length).toBe(2);
+  expect(
+    jsoq
+      .from(json)
+      .in('age', [31, 37])
+      .done().length,
+  ).toBe(2);
 });
 
 test('last', () => {
-  expect(jsoq.from(json).last().done().length).toBe(1);
-  expect(jsoq.from(json).last(2).done().length).toBe(2);
+  expect(
+    jsoq
+      .from(json)
+      .last()
+      .done().length,
+  ).toBe(1);
+  expect(
+    jsoq
+      .from(json)
+      .last(2)
+      .done().length,
+  ).toBe(2);
 });
 
 test('nth', () => {
@@ -25,11 +60,31 @@ test('nth', () => {
 });
 
 test('skip', () => {
-  expect(jsoq.from(json).skip(2).done().length).toBe(4);
+  expect(
+    jsoq
+      .from(json)
+      .skip(2)
+      .done().length,
+  ).toBe(4);
 });
 
 test('where', () => {
-  expect(jsoq.from(json).where({ gender: 'male' }).done().length).toBe(3);
-  expect(jsoq.from(json).where('isActive').done().length).toBe(2);
-  expect(jsoq.from(json).where((o: any) => o.eyeColor === 'green' || o.age === 37).done().length).toBe(2);
+  expect(
+    jsoq
+      .from(json)
+      .where({ gender: 'male' })
+      .done().length,
+  ).toBe(3);
+  expect(
+    jsoq
+      .from(json)
+      .where('isActive')
+      .done().length,
+  ).toBe(2);
+  expect(
+    jsoq
+      .from(json)
+      .where((o: any) => o.eyeColor === 'green' || o.age === 37)
+      .done().length,
+  ).toBe(2);
 });
--- src/__tests__/manipulation.test.ts
+++ src/__tests__/manipulation.test.ts
@@ -2,12 +2,41 @@ import jsoq from '../index';
 const json = require('./data.json');
 
 test('order', () => {
-  expect(jsoq.from(json).order('index asc').done()[0].index).toBe(0);
-  expect(jsoq.from(json).order('index desc').done()[0].index).toBe(5);
-  expect(jsoq.from(json).order('gender asc', 'age desc').done()[0].age).toBe(37);
+  expect(
+    jsoq
+      .from(json)
+      .order('index asc')
+      .done()[0].index,
+  ).toBe(0);
+  expect(
+    jsoq
+      .from(json)
+      .order('index desc')
+      .done()[0].index,
+  ).toBe(5);
+  expect(
+    jsoq
+      .from(json)
+      .order('gender asc', 'age desc')
+      .done()[0].age,
+  ).toBe(37);
 });
 
 test('select', () => {
-  expect(Object.keys(jsoq.from(json).select('bla').done()[0]).length).toBe(0);
-  expect(Object.keys(jsoq.from(json).select('index', 'picture').done()[0]).length).toBe(2);
+  expect(
+    Object.keys(
+      jsoq
+        .from(json)
+        .select('bla')
+        .done()[0],
+    ).length,
+  ).toBe(0);
+  expect(
+    Object.keys(
+      jsoq
+        .from(json)
+        .select('index', 'picture')
+        .done()[0],
+    ).length,
+  ).toBe(2);
 });
